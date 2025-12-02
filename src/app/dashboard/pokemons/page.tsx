ff81906262f900f0f32423e94be1f8f34c43fda2
Luis Medina Coca <luismedinacoca@gmail.com>
2025-12-02 15:00:00 -0300

    section05-lecture051: Data Fetching - Next13+

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,14 @@
-export default function PokemonsPage() {
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(`http://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const pokemonData = await data.json();
+  return pokemonData;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
   return (
     <div>
-      <h1>Hello Page Pokemons</h1>
+      <h1>{JSON.stringify(pokemons)}</h1>
     </div>
   );
 }
