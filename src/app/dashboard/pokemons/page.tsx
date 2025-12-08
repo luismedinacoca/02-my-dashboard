1c3305717cc887f57244c3475e0b977252389acd
Luis Medina Coca <luismedinacoca@gmail.com>
2025-12-08 18:00:52 -0300

    section05-lecture056: Next - Error Page

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -12,6 +12,8 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => 
     id: pokemon.url.split("/").at(-2)!,
     name: pokemon.name,
   }));
+  //throw new Error("This error must not happen 💣 🔥");
+
   return pokemons;
 };
 
